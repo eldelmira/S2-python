69151ab1037dbb4fab388c74d007c245ffea2817
yeh <Laurent.Yeh@uvsq.fr>
2024-03-26 09:03:07 +0000

    ma modif

--- TD/TD3/mytest.py
+++ TD/TD3/mytest.py
@@ -24,10 +24,13 @@ frame0.grid()
 #def create_cercle(x1,x2,y1,y2):
 
 
-
+# pour changer les couleurs des bulles
 def modification():
     Lescouleurs = Variable
     Lescouleurs = ['white', 'red','yellow', "green",'lightpink', 'lightblue']
+
+
+
 # Label 1, du nom de l'utilisateur
 label1 = Label(frame0, text="Name : ", font="cursive")
 label1.grid(row=0,column=0)
@@ -39,20 +42,30 @@ user.grid(row=0, column=1)
 label2 = Label(frame0, text="Password : ", font="cursive")
 label2.grid(row=1,column=0)
 
-# Entry 1, pour écrire le mot de passe de l'utilisateur
+# Entry 2, pour écrire le mot de passe de l'utilisateur
 password =  Entry(frame0, text="Your password", border = "5px" )
 password.grid(row=1, column=1)
 
 # Un lien pour réins
 c1 = Label(frame0, text="Forgot password ?", fg="blue")
-c1.grid(row= 2, column=1)
+c1.grid(row= 4, column=1)
 
 # Première button pour valider
 btn = Button(frame0,text = "Valider")
 btn.grid(row=3, column= 1)
 
+# Label 3, du mot de passe 
+label2 = Label(frame0, text=" Confirmation du Password : ", font="cursive")
+label2.grid(row=2,column=0)
+
+# Entry 3, pour écrire le mot de passe de l'utilisateur
+password_con =  Entry(frame0, text="Your password", border = "5px" )
+password_con.grid(row=1, column=1)
 # Deuxième button pour annuler
-btn = Button(frame0,text = "Annuler")
-btn.grid(row = 3, column=2)
+btn = Button(frame0,text = "Annuler", command = quit)
+btn.grid(row=3, column=1)
+
+# conditions de mot de passe et email
+
 
 window.mainloop()
